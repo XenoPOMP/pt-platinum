bfdfdcc744261774226903e22fdc5ce6b4126ad9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-06-03 18:57:45 +0300

    Update ThemeProvider.tsx

--- src/assets/providers/ThemeProvider/ThemeProvider.tsx
+++ src/assets/providers/ThemeProvider/ThemeProvider.tsx
@@ -7,13 +7,7 @@ import { ProviderProps } from '@providers/Provider.props';
 import styles from './ThemeProvider.module.scss';
 
 const ThemeProvider: FC<ProviderProps> = ({ children }) => {
-	const classContext = useContext(BodyClassnameContext);
-
-	useEffect(() => {
-		classContext.addClassName([styles.themes, styles.dark]);
-	}, []);
-
-	return <>{children}</>;
+	return <div className={cn(styles.themes, styles.dark)}>{children}</div>;
 };
 
 export default ThemeProvider;
